417ce16e7e74c0db74235870c813b3589a8cdd35
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2025-05-01 21:35:58 -0600

    mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const todo = e.target["new-task-description"].value;
+  console.log(todo);
+  handleToDo(todo);
 });
+});
+function handleToDo(todo){
+let p = document.createElement('p')
+let btn = document.createElement('button')
+p.textContent = todo;
+document.querySelector('#list').appendChild(p)
+}
+function handleToDo(todo) {
+ 
+  let li = document.createElement("li");
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  let taskText = document.createTextNode(` ${todo}`);
+  li.appendChild(checkbox);
+  li.appendChild(taskText);
+
+  document.querySelector("#tasks").appendChild(li);
+}
+
+
